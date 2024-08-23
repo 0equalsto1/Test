5f039a5fbe72b46b188a1e10efa5bc73d397adf0
mannu <0equalsto1@gmail.com>
2024-08-23 18:25:12 +0530

    up

--- src/hashId/HashIdTest.java
+++ src/hashId/HashIdTest.java
@@ -4,7 +4,7 @@ import org.hashids.Hashids;
 
 public class HashIdTest {
 	public static void main(String[] args) {
-		Hashids hashids = new Hashids("gaanagems",12);
+		Hashids hashids = new Hashids("appgems",12);
 		String hash = hashids.encode(100);
 		System.out.println(hash);
 		System.out.println(hashids.decode(hash)[0]);
